f2984a6691f7c320826727b46f7e54010bd5d3fd
Minjie Xu <madjxatw@gmail.com>
2026-01-08 18:03:58 +0800

    fix(alpha): pick config files

--- lua/lazyvim/plugins/extras/ui/alpha.lua
+++ lua/lazyvim/plugins/extras/ui/alpha.lua
@@ -26,7 +26,7 @@ return {
         dashboard.button("n", " " .. " New file",        [[<cmd> ene <BAR> startinsert <cr>]]),
         dashboard.button("r", " " .. " Recent files",    [[<cmd> lua LazyVim.pick("oldfiles")() <cr>]]),
         dashboard.button("g", " " .. " Find text",       [[<cmd> lua LazyVim.pick("live_grep")() <cr>]]),
-        dashboard.button("c", " " .. " Config",          "<cmd> lua LazyVim.pick.config_files()() <cr>"),
+        dashboard.button("c", " " .. " Config",          [[<cmd> lua LazyVim.pick.config_files()() <cr>]]),
         dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
         dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
         dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
